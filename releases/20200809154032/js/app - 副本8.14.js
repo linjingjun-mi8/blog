93f36bf256686187dcc692439da6c16e6457f379
YJLAugus <yesyjl@163.com>
2020-08-14 16:51:11 +0800

    更新

--- releases/20200809154032/js/app - 副本8.14.js
+++ releases/20200809154032/js/app - 副本8.14.js
@@ -3012,7 +3012,7 @@ webpackJsonp([1], {
                                     return t.clickOpenArticle(e.url)
                                 }
                             }
-                        }, [i("div", {
+                        }, [i("img", {
                             staticClass: "pub-face panel-right-img-style",
                             attrs: {
                                 src: e.img

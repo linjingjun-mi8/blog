da6e3da5634d7227591dba34069bb315151a4891
YJLAugus <yesyjl@163.com>
2020-09-21 17:54:15 +0800

    更新

--- releases/20200809154032/js/app - 实验成功一次.js
+++ releases/20200809154032/js/app - 实验成功一次.js
@@ -83,18 +83,6 @@ webpackJsonp([1], {
                 headBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/ing/autorbimg.jpg",
                 bigBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/body/background.jpg",
                 aboutmeHtml: " <img src='https://cjunn.gitee.io/blog_theme_atum/img/ing/aboutme.jpg'/>",
-				aboutCouple: "<div class="textwidget">
-<div id="lovexhj" style="width: 100%; height: 100px; text-align: center; font-size: 1rem;">         
-<div id="lovexhjImage" style="width: 220px; margin: 0 auto;">
-
-<img src="https://txcdn.lingn.top/blog/toutu/she.png" alt="love" style="width: 60px; border-radius: 50%;">
-
-<img src="https://txcdn.lingn.top/blog/toutu/z.gif" alt="love" style="width: 60px; border-radius: 50%;">
-
-<img src="https://txcdn.lingn.top/blog/toutu/me.png" alt="love" style="width: 60px; border-radius: 50%;">
-</div>
-<p id="lovexhjSitetime" style="font-size: 0.8rem;    margin-top: 16px;  background: linear-gradient(to right, red, blue);-webkit-background-clip: text;color: transparent;">我们相恋了0年125天2小时26分钟16秒啦</p>  </div>
-</div>",
                 blogUrlPre: "https://www.cnblogs.com/",
 					faceIcon: [{
                     name: "头条",
@@ -4628,8 +4616,7 @@ webpackJsonp([1], {
                         emailImg: W("/img/ing/email.png"),
                         qqImg: W("/img/ing/qq.png"),
                         githubImg: W("/img/ing/github.png"),
-                        aboutmeHtml: g.aboutmeHtml,
-						aboutCouple: g.aboutCouple
+                        aboutmeHtml: g.aboutmeHtml
                     }
                 }
             },
@@ -4700,9 +4687,9 @@ webpackJsonp([1], {
                     }, [t._v("777")]), t._v(" "), i("div", {
                         staticClass: "about-me-body",
                         domProps: {
-                            innerHTML: t._s(t.aboutCouple)
+                            innerHTML: t._s(t.aboutmeHtml)
                         }
-                    }, [t._v("\n        " + t._s(t.aboutCouple) + "\n      ")])]), 
+                    }, [t._v("\n        " + t._s(t.aboutmeHtml) + "\n      ")])]), 
 					
 					i("div", {
                         staticClass: "about-me-wrap"

10526c4c2a996f3e2762c212b81184830f2e2c34
YJLAugus <yesyjl@163.com>
2020-09-21 20:27:28 +0800

    更新

--- releases/20200809154032/js/vendor.js
+++ releases/20200809154032/js/vendor.js
@@ -58,47 +58,4 @@ function(e){var t,n,r,a,i,o,s,l,c,_,d,u,p,m,E,g,S,f,T,b="sizzle"+1*new Date,h=e.
  
  
  
- function secondToDate(second) {
-     if (!second) {
-         return 0;
-     }
-     var time = new Array(0, 0, 0, 0, 0);
-     if (second >= 365 * 24 * 3600) {
-        time[0] = parseInt(second / (365 * 24 * 3600));
-        second %= 365 * 24 * 3600;
-    }
-    if (second >= 24 * 3600) {
-        time[1] = parseInt(second / (24 * 3600));
-        second %= 24 * 3600;
-    }
-    if (second >= 3600) {
-        time[2] = parseInt(second / 3600);
-        second %= 3600;
-    }
-    if (second >= 60) {
-        time[3] = parseInt(second / 60);
-        second %= 60;
-    }
-    if (second > 0) {
-        time[4] = second;
-    }
-    return time;
-};
-function setTime() {
-         // 博客创建时间秒数，时间格式中，月比较特殊，是从0开始的，所以想要显示5月，得写4才行，如下
-         var create_time = Math.round(new Date(Date.UTC(2020, 6, 27, 11, 06, 16)).getTime() / 1000);// 当前时间秒数,增加时区的差异
-         var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
-         currentTime = secondToDate((timestamp - create_time));
-         if (currentTime[0]==0){
-             currentTimeHtml = currentTime[1] + '天'+ currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
-         }else{
-             currentTimeHtml = currentTime[0] + '年' + currentTime[1] + '天' + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
-         }
-         // 兼容pjax，当htmer_time存在时输出，否则清空计时器
-         if (document.getElementById("elapseClock")){
-             document.getElementById("elapseClock").innerHTML = currentTimeHtml;
-         }else{
-              clearInterval(timer);
-         }
-}
-var timer = setInterval(setTime, 1000);
+ 

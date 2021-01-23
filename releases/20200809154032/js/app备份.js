bc9d102cdb00ca3c7c3e08ecc81f938f2d118b10
YJLAugus <yesyjl@163.com>
2021-01-23 13:24:40 +0800

    update

--- releases/20200809154032/js/app备份.js
+++ releases/20200809154032/js/app备份.js
@@ -83,7 +83,7 @@ webpackJsonp([1], {
                 headBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/ing/autorbimg.jpg",
                 bigBackImg: "https://cjunn.gitee.io/blog_theme_atum/img/body/background.jpg",
                 aboutmeHtml: "<img src='https://cjunn.gitee.io/blog_theme_atum/img/ing/aboutme.jpg'/>",
-				aboutCouple: "<div class='textwidget'><div id='lovenyh' style='width: 100%; height: 100px; text-align: center; font-size: 1rem;'> <div id='lovenyhImage' style='width: 220px; margin: 0 auto;'><img src='https://yjlaugus.gitee.io/blog/img/body/ll.jpg' alt='love' style='width: 60px; border-radius: 50%;'><img src='https://yjlaugus.gitee.io/blog/img/body/z.webp' alt='love' style='width: 60px; border-radius: 50%;'><img src='https://yjlaugus.gitee.io/blog/img/body/nn.jpg' alt='love' style='width: 60px; border-radius: 50%;'></div><p id='elapseClock' style='font-size: 0.8rem;    margin-top: 16px;  background: linear-gradient(to right, red, blue);-webkit-background-clip: text;color: transparent;'>我们相恋了(刷新页面看天数呀)</p></div></div>",
+				aboutCouple: "<div class='textwidget'><div id='lovenyh' style='width: 100%; height: 100px; text-align: center; font-size: 1rem;'> <div id='lovenyhImage' style='width: 220px; margin: 0 auto;'><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/ll.jpg' alt='love' style='width: 60px; border-radius: 50%;'></a><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/z.webp' alt='love' style='width: 60px; border-radius: 50%;'></a><a href='https://yjlaugus.gitee.io/loveniuniu/' target='_blank'><img src='https://yjlaugus.gitee.io/blog/img/body/nn.jpg' alt='love' style='width: 60px; border-radius: 50%;'></a></div><p id='elapseClock' style='font-size: 0.8rem;    margin-top: 16px;  background: linear-gradient(to right, red, blue);-webkit-background-clip: text;color: transparent;'>我们相恋了(刷新页面看天数呀)</p></div></div>",
                 blogUrlPre: "https://www.cnblogs.com/",
 					faceIcon: [{
                     name: "头条",
@@ -2870,7 +2870,7 @@ webpackJsonp([1], {
                     return {
                         list: [{
                             img: "https://yjlaugus.gitee.io/blog/img/body/ad1.jpg",
-                            url: "https://yjlaugus.gitee.io/index"
+                            url: "https://yjlaugus.gitee.io"
                         }]
                     }
                 }
